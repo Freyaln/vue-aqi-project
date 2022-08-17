b48dfedce00f7afd3b63588412ed8afbf8c1c207
Freyaln <freyling.aurelien@gmail.com>
2022-08-17 13:58:13 +0200

    Project initialization

---
+++ vue-aqi-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>Vuejs AQI app</h1>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
